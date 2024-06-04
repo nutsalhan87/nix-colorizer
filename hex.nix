b65bebee699011dcc04cb1db1d148d04cddf9287
nutsalhan87 <nutsalhan87@yandex.ru>
2024-06-04 17:49:50 +0300

    moved some functions to utils.nix

--- hex.nix
+++ hex.nix
@@ -1,21 +1,13 @@
 rgb255ToRgb:
 let
   utils = import ./utils.nix;
-  hexEnum =
-    (utils.merge (map 
-      (x: { "${builtins.toString x}" = x; }) 
-      (builtins.genList (x: x) 10)
-    ))
-    // { "a" = 10; "A" = 10; "b" = 11; "B" = 11; "c" = 12; "C" = 12; "d" = 13; "D" = 13; "e" = 14; "E" = 14; "f" = 15; "F" = 15; };
-  hexToDecimal = hex: builtins.foldl' (a: b: a * 16 + b) 0 
-    (map (x: hexEnum.${x}) (utils.symbols hex));
 in
 {
   to = rec {
     rgb255 = hex: {
-      r = hexToDecimal (builtins.substring 1 2 hex);
-      g = hexToDecimal (builtins.substring 3 2 hex);
-      b = hexToDecimal (builtins.substring 5 2 hex);
+      r = utils.hexToDecimal (builtins.substring 1 2 hex);
+      g = utils.hexToDecimal (builtins.substring 3 2 hex);
+      b = utils.hexToDecimal (builtins.substring 5 2 hex);
     };
     rgb = hex: rgb255ToRgb (rgb255 hex);
   };
--- utils.nix
+++ utils.nix
@@ -1,3 +1,13 @@
+let
+  merge = listOfAttrSets: builtins.foldl' (a: b: a // b) { } listOfAttrSets;
+  decEnum = 
+    (merge (map 
+      (x: { "${builtins.toString x}" = x; }) 
+      (builtins.genList (x: x) 10)
+    ));
+  symbols = str: builtins.genList (p: builtins.substring p 1 str) (builtins.stringLength str);
+  repeatString = str: n: if n == 0 then "" else str + (repeatString str (n - 1));
+in
 rec {
   max = x: y: if x > y then x else y;
   min = x: y: if x < y then x else y;
@@ -16,7 +26,27 @@ rec {
   in 
     if (x - floored) > (ceiled - x) then ceiled else floored;
 
-  symbols = str: builtins.genList (p: builtins.substring p 1 str) (builtins.stringLength str);
+  hexToDecimal = hex: let 
+    hexEnum = decEnum
+      // { "a" = 10; "A" = 10; "b" = 11; "B" = 11; "c" = 12; "C" = 12; "d" = 13; "D" = 13; "e" = 14; "E" = 14; "f" = 15; "F" = 15; };
+  in
+    builtins.foldl' (a: b: a * 16 + b) 0 (map (x: hexEnum.${x}) (symbols hex));
 
-  merge = listOfAttrSets: builtins.foldl' (a: b: a // b) { } listOfAttrSets;
+  decimalToHex = dec: let
+    revHexEnum = decEnum // { "10" = "A"; "11" = "B"; "12" = "C"; "13" = "D"; "14" = "E"; "15" = "F"; };
+    decimalToHexInner = dec: if dec == 0 
+      then ""
+      else decimalToHexInner (dec / 16) + builtins.toString revHexEnum.${builtins.toString (mod dec 16)};
+    h = decimalToHexInner dec;
+  in
+    if (builtins.stringLength h) == 0 then "0" else h;
+
+  alignedDecimalToHex = dec: minNums: let
+    hex = decimalToHex dec;
+    diff = minNums - (builtins.stringLength hex);
+    zerosNum = if diff <= 0 then 0 else diff;
+  in
+    (repeatString "0" zerosNum) + hex; 
+
+  inherit merge;
 }
